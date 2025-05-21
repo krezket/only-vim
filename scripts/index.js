b36b5ab7afdeeb6fbf6665de39abfe6f9850deae
Anthony <anthonyguerrero62@outlook.com>
2025-05-20 21:58:50 -0700

    making commands

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,34 @@
+let ctrlPressed = false;
+
 document.addEventListener("keydown", function(event) {
     if (event.key ==="n") {
         console.log("n pressed");
     }; 
 });
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === 'Control') {
+        ctrlPressed = true;
+        // console.log("pressed");
+    }
+    if (ctrlPressed && event.key ==="d") {
+        event.preventDefault();
+        console.log("ctrl + d pressed");
+    }; 
+});
+
+document.addEventListener("keydown", function(event) {
+    if (ctrlPressed) {
+        if (event.key === "u") {
+            event.preventDefault();
+            console.log("ctrl + u pressed");
+        };
+    }; 
+});
+
+document.addEventListener("keyup", function(event) {
+    if (event.key === "Control") {
+        ctrlPressed = false;
+        // console.log("released");
+    }
+})
